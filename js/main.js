fdd473aa89b4f5f33bcab5244cd9513b1954f8e9
innazozulia <innazozulia@outlook.com>
2021-09-25 18:04:49 +0300

    adding files

---
+++ js/main.js
@@ -0,0 +1,12 @@
+let menu = document.querySelector('.fa-bars');
+let navigation = document.querySelector('.nav');
+
+menu.addEventListener('click', function(){
+  menu.classList.toggle('fa-times');
+  navigation.classList.toggle('nav-toggle');
+});
+
+window.addEventListener('scroll',  () => {
+  menu.classList.remove('fa-times');
+  navigation.classList.remove('nav-toggle');
+});
